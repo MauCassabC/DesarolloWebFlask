4cd90e9608e7a7e165e8237c871751993bb2ac6c
MauCassabC <a01025475@itesm.mx>
2021-10-13 21:58:44 -0500

    Termiando el proyecto

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask.wrappers import Request
+from pymongo import MongoClient, cursor
+
+con = MongoClient("localhost", 27017)
+db = con.Escuala
+cuentas = db.alumno
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    cursor = cuentas.find()
+    for doc in cursor:
+        print(doc)
+
     return render_template("/login.html", data="Mau")
 
 
 @app.route('/login', methods=["POST"])
-def login():
+def login():    
     password = request.form["password"]
     return "Esta es tu password: %s" %password 
-    
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
